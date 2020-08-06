2d9f8fc313635b1349d6407e63c45b9ba47e71eb
George Aristy <george.aristy@securekey.com>
2020-08-06 00:05:08 -0400

    chore: CI
    Signed-off-by: George Aristy <george.aristy@securekey.com>

---
+++ ci/version_var.sh
@@ -0,0 +1,28 @@
+#
+# Copyright SecureKey Technologies Inc. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Release Parameters
+BASE_VERSION=0.1.4
+IS_RELEASE=false
+
+# Project Parameters
+SOURCE_REPO=hub-router
+PKG_NAME=hub-router
+RELEASE_REPO=docker.pkg.github.com/trustbloc/${SOURCE_REPO}
+SNAPSHOT_REPO=docker.pkg.github.com/trustbloc-cicd/snapshot
+
+if [ ${IS_RELEASE} = false ]
+then
+  EXTRA_VERSION=snapshot-$(git rev-parse --short=7 HEAD)
+  PROJECT_VERSION=${BASE_VERSION}-${EXTRA_VERSION}
+  PROJECT_PKG_REPO=${SNAPSHOT_REPO}
+else
+  PROJECT_VERSION=${BASE_VERSION}
+  PROJECT_PKG_REPO=${RELEASE_REPO}
+fi
+
+export HUB_ROUTER_TAG=$PROJECT_VERSION
+export HUB_ROUTER_PKG=${PROJECT_PKG_REPO}/${PKG_NAME}
